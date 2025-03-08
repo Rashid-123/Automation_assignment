4a7f1d9b6f01a40ac5d5d4fb2251969928ba1452
Rashid-123 <shadanrashid786@gmail.com>
2025-03-08 16:47:01 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-// ✅ Apply CORS Middleware
+
 app.use(
   cors({
-    origin: FRONTEND_URL, // Allow requests from the frontend
-    credentials: true, // Allow cookies if needed
+    origin: FRONTEND_URL, 
+    credentials: true, 
   })
 );
 
